abfe397d5ba3fe3f1464093e6938f003cbf0bf0a
8Dweb <mjs@8dweb.co>
2025-01-17 07:13:00 -0500

    Create joomla_find_smtp_info.sh
    Simple search to find a specific smtp server in Joomla configuration.php files. This probably needs to be heavily modified and updated.

---
+++ joomla_find_smtp_info.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Directory to search in
+search_dir="/var/www/"
+
+# Strings to search and replace
+search_string="smtp15.cmsdefender.com"
+replace_string="mail.cmsdefender.com"
+
+# Output file
+output_file="replaced_files.txt"
+
+# Clear the output file if it exists
+> "$output_file"
+
+# Find all configuration.php files and replace the string
+find "$search_dir" -type f -name "configuration.php" | while read -r file; do
+    if grep -q "$search_string" "$file"; then
+        # Replace the string in the file
+        sed -i "s/$search_string/$replace_string/g" "$file"
+        echo "Replaced '$search_string' with '$replace_string' in: $file"
+        echo "$file" >> "$output_file"
+    fi
+done
+
+echo "List of modified files saved to $output_file"
